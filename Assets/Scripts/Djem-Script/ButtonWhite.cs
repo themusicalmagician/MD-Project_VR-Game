91990b1e8f77d03bbcf3de767c951c8247fb7f20
Djemtheman05 <d.thijs@sintlucasedu.nl>
2023-05-30 14:50:20 +0200

    Fixed it again

--- Assets/Scripts/Djem-Script/ButtonWhite.cs
+++ Assets/Scripts/Djem-Script/ButtonWhite.cs
@@ -8,16 +8,12 @@ public class ButtonWhite : MonoBehaviour
     public AudioSource hit;
     public static bool point = false;
     Collider collision;
-    public TextMeshProUGUI score;
-    int scoreCount = 0;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("hands"))
         {
             hit.PlayDelayed(0);
-            scoreCount += 1;
-            Debug.Log(scoreCount);
             gameObject.GetComponent<Renderer>().material.color = Color.white;
             gameObject.GetComponent<Collider>().enabled = false;
         }
--- Assets/Scripts/Djem-Script/HandsTrigger.cs
+++ Assets/Scripts/Djem-Script/HandsTrigger.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HandsTrigger : MonoBehaviour
 {
+    public TextMeshProUGUI score;
+    int scoreCount = 0;
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ButtonArcade"))
+        {
+            scoreCount += 1;
+            score.text = scoreCount.ToString();
+            Debug.Log(scoreCount);
+        }
+    }
 }
