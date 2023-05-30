e0a62ff5c4d3e3283d7ddd047215b19ac6b5be1c
Djemtheman05 <d.thijs@sintlucasedu.nl>
2023-05-30 14:40:15 +0200

    fixed buttons

--- Assets/Scripts/Djem-Script/ButtonWhite.cs
+++ Assets/Scripts/Djem-Script/ButtonWhite.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ButtonWhite : MonoBehaviour
 {
+    public AudioSource hit;
+    public static bool point = false;
+    Collider collision;
+    public TextMeshProUGUI score;
+    int scoreCount = 0;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("hands"))
         {
-            RandomButton.changeWhite();
+            hit.PlayDelayed(0);
+            scoreCount += 1;
+            Debug.Log(scoreCount);
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+            gameObject.GetComponent<Collider>().enabled = false;
         }
     }
 }
--- Assets/Scripts/Djem-Script/HandsTrigger.cs
+++ Assets/Scripts/Djem-Script/HandsTrigger.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class HandsTrigger : MonoBehaviour
 {
-    public AudioSource hit;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("ButtonArcade"))
-        {
-            hit.PlayDelayed(0);
-        }
-    }
 }
