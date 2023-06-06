61bf461bebf31d87b3becd67f4474fb429ce44dd
Djemtheman05 <d.thijs@sintlucasedu.nl>
2023-06-06 19:51:10 +0200

    gex

--- Assets/Scripts/Djem-Script/RedButton.cs
+++ Assets/Scripts/Djem-Script/RedButton.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class RedButton : MonoBehaviour
 {
     public int PointsToGet;
+    public bool ButtonRoom;
+    public bool YeetRoom;
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.CompareTag("hands"))
         {
-            if(ScoreRing.scoreCount != PointsToGet)
+            if(ScoreRing.scoreCount != PointsToGet && YeetRoom == true)
             {
                 Spooky.jumpscare = true;
                 ScoreRing.scoreCount = 0;
             }
-            if (HandsTrigger.scoreCount != PointsToGet)
+            if (HandsTrigger.scoreCount != PointsToGet && ButtonRoom == true)
             {
                 Spooky.jumpscare = true;
-                HandsTrigger.scoreCount = 0;    
+                HandsTrigger.scoreCount = 0;
             }
+        }
+    }
+    private void Update()
+    {
+        if (YeetRoom == true && ButtonRoom == true || YeetRoom == false && ButtonRoom == false)
+        {
+            Debug.LogError("Select only one level!");
         }
     }
 }
