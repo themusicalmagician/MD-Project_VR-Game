8b1e1329fa728f6c59b6fdcefc96396c423c4b33
Djemtheman05 <d.thijs@sintlucasedu.nl>
2023-06-14 10:11:33 +0200

    test

--- Assets/Scripts/Djem-Script/PortalSpawner.cs
+++ Assets/Scripts/Djem-Script/PortalSpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PortalSpawner : MonoBehaviour
 {
     public GameObject Portal;
+    float Y;
 
     void Start()
     {
@@ -13,11 +14,18 @@ public class PortalSpawner : MonoBehaviour
 
     IEnumerator PositionPortal()
     {
-        float SpawnTime = (Random.Range(1.5f, 4));
-        float Z = (Random.Range(0.4f, -0.4f));       
-        float Y = (Random.Range(-0.4f, 0.2f));
+        float SpawnTime = (Random.Range(1.5f, 3));
+        int rand = Random.Range(0, 2);
+        if (rand == 0)
+        {
+            Y = -0.4f;
+        }
+        if (rand == 1)
+        {
+            Y = 0;
+        }
         yield return new WaitForSeconds(SpawnTime);
-        transform.localPosition = new Vector3(-1, Y, Z);
+        transform.localPosition = new Vector3(-1, Y, 0);
         GameObject instantiatedObject = Instantiate(Portal, transform.position, Quaternion.identity);
         StartCoroutine(PositionPortal());
     }
